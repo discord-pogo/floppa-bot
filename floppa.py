03f7bb27e6a928a1ea4a81e3473652a548c804d8
nooney <hyperblast@protonmail.ch>
2021-06-19 00:50:01 +0200

    floppa.py: initialize

---
+++ floppa.py
@@ -0,0 +1,17 @@
+import discord
+from discord_slash import SlashCommand
+
+
+token = "token_here"
+client = discord.Client(intents=discord.Intents.all())
+slash = SlashCommand(client, sync_commands=True)
+guild_ids = [733665939198967852]
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@slash.slash(name="test",  guild_ids=guild_ids)
+async def _test(ctx):
+    await ctx.send("Test!")
+
+client.run(token)
