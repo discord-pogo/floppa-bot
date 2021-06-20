cb1a47e0867d05e19e95f2d82ea3d00b4d49bcbe
nooney <hyperblast@protonmail.ch>
2021-06-20 13:49:54 +0200

    floppa.py: change token system to environment var

--- floppa.py
+++ floppa.py
@@ -1,17 +1,17 @@
+import os
 import discord
 from discord_slash import SlashCommand
 
+token = os.getenv("FLOPPA_TOKEN")
 
-token = "token_here"
 client = discord.Client(intents=discord.Intents.all())
 slash = SlashCommand(client, sync_commands=True)
 guild_ids = [733665939198967852]
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
-
 @slash.slash(name="test",  guild_ids=guild_ids)
 async def _test(ctx):
-    await ctx.send("Test!")
+    await ctx.send("kys!")
 
 client.run(token)
