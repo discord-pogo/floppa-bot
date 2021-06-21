a8dff2737e5fbb70b2e7402ca3498188f70d83a7
nooney <hyperblast@protonmail.ch>
2021-06-21 12:11:12 +0200

    floppa.py: create a link command

--- floppa.py
+++ floppa.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option
 
 token = os.getenv("FLOPPA_TOKEN")
 
@@ -10,8 +11,10 @@ guild_ids = [733665939198967852]
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
-@slash.slash(name="test",  guild_ids=guild_ids)
-async def _test(ctx):
-    await ctx.send("kys!")
+@slash.slash(name="link", description="Add MC name to database", options=[create_option(name="account", description="Minecraft account name", option_type=3, required=True)],  guild_ids=guild_ids)
+async def _link(ctx, account: str):
+    if " " in account:
+        await ctx.send("Account failed to link, contains space")
+    else: await ctx.send("✅ Account linked: " + account)
 
 client.run(token)
